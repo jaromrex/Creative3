9472c51d6d0ac9391ff7ccb1cae4952b23690118
Jarom Christensen <jaromchristensen5@gmail.com>
2017-03-03 00:56:34 +0000

    update functionality

--- app.js
+++ app.js
@@ -3,31 +3,17 @@ angular.module('app', [])
 
 function MainCtrl($scope){
 
-	$scope.payments = [
-		{
-			name: 'Lunch Tuesday',
-			price: 12,
-			active:true
-		},{
-			name: 'Speeding Ticket',
-			price: 200,
-			active:false
-		},{
-			name: 'Grocery Shopping',
-			price: 80,
-			active:false
-		},{
-			name: 'New Pants',
-			price: 30,
-			active:false
-		}
-	];
+	$scope.payments = [];
+    
+    $scope.addPayment = function() {
+        $scope.payments.push({name:$scope.paymentInput, price:$scope.costInput, active:true});
+        $scope.paymentInput = '';
+        $scope.costInput = '';
+    }
 
 	$scope.toggleActive = function(s){
 		s.active = !s.active;
 	};
-
-	// Helper method for calculating the total price
 
 	$scope.total = function(){
 
